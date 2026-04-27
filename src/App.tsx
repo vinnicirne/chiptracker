3441902ef7af2ee3dfea59fc706dcbc4671cd90e
Vinicius <viniciuscirne@gmail.com>
2026-04-27 09:56:34 -0300

    Build Fix: Adicionado import de React para suportar Fragmentos

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { useState, useEffect, type FormEvent } from 'react';
+import React, { useState, useEffect, type FormEvent } from 'react';
 import { 
   Activity, 
   AlertTriangle, 
